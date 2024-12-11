eaf28e354dbbe88b93e8e6128564d43b37a782b3
younusraza909 <younusraza909@gmail.com>
2024-12-11 07:26:49 +0500

    hudration issue fixed

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  if (!isLoaded) return;
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
