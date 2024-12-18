1914da6716a8f83f096d614f4bbe9773db1a0be5
younusraza909 <younusraza909@gmail.com>
2024-12-18 23:48:55 +0500

    removed console

--- src/hooks/use-nav.tsx
+++ src/hooks/use-nav.tsx
@@ -3,15 +3,9 @@ import { usePathname } from "next/navigation";
 export const usePaths = () => {
   const pathname = usePathname();
 
-  console.log("pathname", pathname);
-
   const path = pathname.split("/");
 
-  console.log("path", path);
-
   let page = path[path.length - 1];
-
-  console.log("page", page);
 
   return {
     page,
