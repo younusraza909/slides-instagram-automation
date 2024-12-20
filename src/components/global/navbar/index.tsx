7bf4c0f9091bb1d8e7bd1b37e6abadcadd962292
younusraza909 <younusraza909@gmail.com>
2024-12-20 07:55:58 +0500

    Sidebar for mobile

--- src/components/global/navbar/index.tsx
+++ src/components/global/navbar/index.tsx
@@ -5,6 +5,13 @@ import { usePaths } from "@/hooks/use-nav";
 import React from "react";
 import Sheet from "../sheet";
 import { Menu } from "lucide-react";
+import Items from "../sidebar/items";
+import { Separator } from "@/components/ui/separator";
+import ClerkAuthState from "../clerk-auth-state";
+import { HelpDuoToneWhite } from "@/icons";
+import { SubscriptionPlan } from "../subscription-plan";
+import UpgradeCard from "../sidebar/upgrade";
+import { LogoSmall } from "@/svgs/logo-small";
 
 type Props = {
   slug: string;
@@ -20,7 +27,37 @@ const Navbar = ({ slug }: Props) => {
       <div className="flex flex-col">
         <div className="flex gap-x-3 lg:gap-x-5 justify-end">
           <span className="lg:hidden flex items-center flex-1 gap-x-2">
-            <Sheet trigger={<Menu />} className="lg:hidden"></Sheet>
+            <Sheet side="left" trigger={<Menu />} className="lg:hidden">
+              <div className="flex flex-col gap-y-5 w-full h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
+                <div className="flex gap-x-2 items-center p-5 justify-center">
+                  <LogoSmall />
+                </div>
+                <div className="flex flex-col py-3">
+                  <Items page={page} slug={slug} />
+                </div>
+                <div className="px-16">
+                  <Separator
+                    orientation="horizontal"
+                    className="bg-[#333336]"
+                  />
+                </div>
+                <div className="px-3 flex flex-col gap-y-5">
+                  <div className="flex gap-x-2">
+                    <ClerkAuthState />
+                    <p className="text-[#9B9CA0]">Profile</p>
+                  </div>
+                  <div className="flex gap-x-3">
+                    <HelpDuoToneWhite />
+                    <p className="text-[#9B9CA0]">Help</p>
+                  </div>
+                </div>
+                <SubscriptionPlan type="FREE">
+                  <div className="flex-1 flex flex-col justify-end">
+                    <UpgradeCard />
+                  </div>
+                </SubscriptionPlan>
+              </div>
+            </Sheet>
           </span>
         </div>
       </div>
--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -16,37 +16,8 @@ type Props = {
 const Sidebar = ({ slug }: Props) => {
   const { page } = usePaths();
   return (
-    <div
-      className="w-[250px] 
-  border-[1px]
-  radial 
-  fixed 
-  left-0 
-  lg:inline-block
-  border-[#545454] 
-  bg-gradient-to-b from-[#768BDD] 
-  via-[#171717]
-   to-[#768BDD] 
-   hidden 
-   bottom-0 
-   top-0 
-   m-3 
-   rounded-3xl 
-   overflow-hidden"
-    >
-      <div
-        className="flex flex-col 
-      gap-y-5
-       w-full 
-       h-full 
-       p-3 
-       bg-[#0e0e0e] 
-       bg-opacity-90 
-       bg-clip-padding 
-       backdrop-filter 
-       backdrop--blur__safari 
-       backdrop-blur-3xl"
-      >
+    <div className="w-[250px] border-[1px] radial fixed left-0 lg:inline-block border-[#545454] bg-gradient-to-b from-[#768BDD] via-[#171717] to-[#768BDD] hidden bottom-0 top-0 m-3 rounded-3xl overflow-hidden">
+      <div className="flex flex-col gap-y-5 w-full h-full p-3 bg-[#0e0e0e] bg-opacity-90 bg-clip-padding backdrop-filter backdrop--blur__safari backdrop-blur-3xl">
         <div className="flex gap-x-2 items-center p-5 justify-center">
           <LogoSmall />
         </div>
