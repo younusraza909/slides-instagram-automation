c69774ce1fa9101f5864e8b2091b88f5933d0979
younusraza909 <younusraza909@gmail.com>
2024-12-21 16:03:01 +0500

    Integration Tab UI

---
+++ src/app/(protected)/dashboard/[slug]/integrations/_components/integration-card/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+type Props = {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+  strategy: "INSTAGRAM" | "CRM";
+};
+
+const IntegrationCard = ({ description, icon, strategy, title }: Props) => {
+  return (
+    <div className="border-2 border-[#3352CC] rounded-2xl gap-x-5 p-5 flex items-center justify-between">
+      {icon}
+      <div className="flex flex-col flex-1">
+        <h3 className="text-xl"> {title}</h3>
+        <p className="text-[#9D9D9D] text-base ">{description}</p>
+      </div>
+      <Button
+        onClick={() => {}}
+        className="bg-gradient-to-br text-white rounded-full text-lg from-[#3352CC] font-medium to-[#1C2D70] hover:opacity-70 transition duration-100"
+      >
+        Connected
+      </Button>
+    </div>
+  );
+};
+
+export default IntegrationCard;
