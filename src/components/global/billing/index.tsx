5822ab50e38dc9a99d9018f2b55211fe54c7a947
younusraza909 <younusraza909@gmail.com>
2024-12-21 15:51:32 +0500

    Settings page for billing

--- src/components/global/billing/index.tsx
+++ src/components/global/billing/index.tsx
@@ -4,7 +4,8 @@ import PaymentCard from "./payment-card";
 const Billing = () => {
   return (
     <div className="flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container">
-      <PaymentCard label="PRO" current="PRO" />
+      <PaymentCard current="PRO" label="PRO" />
+      <PaymentCard current="PRO" label="FREE" />
     </div>
   );
 };
