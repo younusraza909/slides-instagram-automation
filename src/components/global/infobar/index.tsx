c578635a883268654bdd47a9ab4857420b3a0a94
younusraza909 <younusraza909@gmail.com>
2024-12-22 21:34:11 +0500

    Update Import

--- src/components/global/infobar/index.tsx
+++ src/components/global/infobar/index.tsx
@@ -15,7 +15,7 @@ import { LogoSmall } from "@/svgs/logo-small";
 import CreateAutomation from "../create-automation";
 import Search from "../search";
 import { Notifications } from "../notifications";
-import MainBreadCrumb from "../main-bread-crumb";
+import MainBreadCrumb from "../bread-crumbs/main-bread-crumb";
 
 type Props = {
   slug: string;
